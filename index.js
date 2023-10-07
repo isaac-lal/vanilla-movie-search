3ebde82782b66538a0260ede36950a2d8626bd70
Isaac Lal <isaaclal124@gmail.com>
2023-10-07 18:34:02 -0400

    Updated omdbapi

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const resultGrid = document.getElementById('result-grid');
 
 // load movies from API
 async function loadMovies(searchTerm) {
-  const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
+  const URL = `http://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
   const res = await fetch(`${URL}`);
   const data = await res.json();
   // console.log(data.Search);
